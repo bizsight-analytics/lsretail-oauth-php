f669c16dc3186fb18cb30cceebcd9aeadf7e321b
bizsight-analytics <119604282+bizsight-analytics@users.noreply.github.com>
2022-12-01 16:56:12 +0000

    added php code title to config

--- config.php
+++ config.php
@@ -1,3 +1,5 @@
+<?php
+
 return [
 	'clientID' => '1701edfe0bb8b3276110bb6d0e359f62c14b7f84ba8ed200b961a7bebf412592',
 	'clientSecret' => 'b67dd9da9ebd4f6ba4f37b529570a94e0f73891f94af51e30c6129d14ca33e2c',
