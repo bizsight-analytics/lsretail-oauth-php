cd8b5a10e435c3d497ad6a5de3c8951d91ff17f6
bizsight-analytics <119604282+bizsight-analytics@users.noreply.github.com>
2022-12-08 16:32:27 +0000

    added buttons and config

--- build-link.php
+++ build-link.php
@@ -5,6 +5,15 @@ $client_id = $config['clientID'];
 $scope = $_POST['scope'];
 $authURL = "https://cloud.lightspeedapp.com/oauth/authorize.php?response_type=code&client_id={$client_id}&scope={$scope}";
 
+$SquareClient_id = $config['SquareClientID'];
+$SquareAuthURL = "https://cloud.lightspeedapp.com/oauth/authorize.php?response_type=code&client_id={$SquareClient_id}&scope={$scope}";
+
+$SumUpClient_id = $config['SumUpClientID'];
+$SumUpAuthURL = "https://cloud.lightspeedapp.com/oauth/authorize.php?response_type=code&client_id={$SumUpClient_id}&scope={$scope}";
+
+$ePOSNowClient_id = $config['ePOSNowClientID'];
+$ePOSNowAuthURL = "https://cloud.lightspeedapp.com/oauth/authorize.php?response_type=code&client_id={$ePOSNowClient_id}&scope={$scope}";
+
 if ($_POST['button'] == 'generate'):
 
 ?>
@@ -22,8 +31,14 @@ if ($_POST['button'] == 'generate'):
 
 <?php
 
-elseif ($_POST['button'] == 'link'):
+elseif ($_POST['button'] == 'Lightspeed'):
 	header("location: {$authURL}");
+elseif ($_POST['button'] == 'Square'):
+	header("location: {$SquareAuthURL}");
+elseif ($_POST['button'] == 'SumUp'):
+	header("location: {$SumUpAuthURL}");
+elseif ($_POST['button'] == 'ePOS Now'):
+	header("location: {$ePOSNowAuthURL}");
 endif;
 
 ?>
--- config.php
+++ config.php
@@ -3,7 +3,13 @@
 return [
 	'clientID' => '1701edfe0bb8b3276110bb6d0e359f62c14b7f84ba8ed200b961a7bebf412592',
 	'clientSecret' => 'b67dd9da9ebd4f6ba4f37b529570a94e0f73891f94af51e30c6129d14ca33e2c',
-	'redirectURI' => 'https://bizsight-oauth-test.azurewebsites.net/temp-token.php'
+	'redirectURI' => 'https://bizsight-oauth-test.azurewebsites.net/temp-token.php',
+	'SquareClientID' => '',
+	'SquareClientSecret' => '',
+	'SumUpClientID' => '',
+	'SumUpClientSecret' => '',
+	'ePOSNowClientID' => '',
+	'ePOSNowClientSecret' => ''
 ];
 
 ?>
