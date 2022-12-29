bae79516a7d953e4faebaa7d0003fe2708dca51c
bizsight-analytics <info@bizsight.co.uk>
2022-12-29 13:35:31 +0000

    square confit and removed error from temptoken

--- temp-token.php
+++ temp-token.php
@@ -39,18 +39,3 @@ if ($err) {
     echo "<pre>$jsonString</pre>";
 }
 ?>
-
-<?php  
-if ($_POST['button'] == 'temptest'):
-    if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')   
-         $url = "https://";   
-    else  
-         $url = "http://";   
-    // Append the host(domain name, ip) to the URL.   
-    $url.= $_SERVER['HTTP_HOST'];   
-    
-    // Append the requested resource location to the URL   
-    $url.= $_SERVER['REQUEST_URI'];    
-      
-    echo $url;  
-  ?>   
