de8240583c40409731688b9b96b7dbe21c04cdd0
bizsight-analytics <119604282+bizsight-analytics@users.noreply.github.com>
2023-01-05 12:25:34 +0000

    added error reporting to lightspeed token page

--- temp-token.php
+++ temp-token.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $config = require 'config.php';
 $tokenURL = "https://cloud.lightspeedapp.com/oauth/access_token.php";
 $tempToken = $_GET['code'];
